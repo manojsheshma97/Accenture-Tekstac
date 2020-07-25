1a9cd097da90ddea4dc48a8c0151df57206ecfe5
itsjustmithun <mitbusiness15@gmail.com>
2020-07-25 14:19:53 +0530

    java

---
+++ Maven/MavenJavaApp/MavenJavaApp/src/main/java/com/build/maven/java/App.java
@@ -0,0 +1,18 @@
+package com.build.maven.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+   /*code getHelloWorld() here */
+   public static String getHelloWorld(){
+       return "Hello World!";
+   }
+   
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
