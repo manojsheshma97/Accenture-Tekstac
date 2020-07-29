d7a9db2b708ac1678f0c850973a92382ba632fe8
itsjustmithun <mitbusiness15@gmail.com>
2020-07-29 13:51:32 +0530

    concatenate

---
+++ C#/StringConcatenate/Program.cs
@@ -0,0 +1,13 @@
+using System;
+class Program 
+{ 
+    static void Main(string[] args) 
+    {
+        //Implement code here
+        Console.WriteLine("Enter first name");
+        String f=Console.ReadLine();
+        Console.WriteLine("Enter last name");
+        String l=Console.ReadLine();
+        Console.Write("Full name : {0} {1}",f,l);
+    }
+} 
